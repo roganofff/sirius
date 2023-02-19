ce91eb7dc1c7e2a8dddcc04fc8c7c4eb32eb73b7
roganofff <matematic1337@gmail.com>
2023-02-20 01:55:59 +0300

    added new task from 2nd presentation

---
+++ programmizm/lesson3/task1_2.py
@@ -0,0 +1,6 @@
+print("What genre of music do you prefer?")
+for i in range(3):
+    reply = input("%s. " % (i + 1))
+    print("Preference taken into account.")
+
+print("Preference system is configured.")
