0d015e10a4c415abcd23e158b9d0d6f0e33405ba
roganoff <matematic1337@gmail.com>
2023-02-27 01:44:43 +0300

    task5_2 solved

---
+++ programmizm/lesson3/task5_2.py
@@ -0,0 +1,6 @@
+discount = 0.1
+price = int(input("Enter price of an item: "))
+
+while price != 0:
+    print("Discount price:", price * discount)
+    price = int(input("Enter price of an item: "))
