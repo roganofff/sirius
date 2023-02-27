adc26e1b81957fd18ccf58ee229d62becd35f299
roganoff <matematic1337@gmail.com>
2023-02-27 11:48:00 +0300

    task4_2 final

--- programmizm/lesson3/task4_2.py
+++ programmizm/lesson3/task4_2.py
@@ -1,8 +1,8 @@
-phrase = ''
-
 print("Leave a review.")
+
+phrase = input()
 while phrase != "off":
-    phrase = input()
     print("Thanks for your voice.")
+    phrase = input()
 
 print("Preference system is configured.")
