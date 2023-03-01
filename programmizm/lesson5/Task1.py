10144880a68f3fd921b6c88974dcc6a0c301f223
roganoff <matematic1337@gmail.com>
2023-03-01 11:41:09 +0300

    task1 solved

---
+++ programmizm/lesson5/Task1.py
@@ -0,0 +1,10 @@
+"""
+Создайте словарь, в котором ключами будут числа от 1 до 10, а значениями эти же числа, возведенные в куб.
+"""
+cubed_numbers = dict()
+
+for i in range(10):
+    cubed_numbers[i + 1] = pow((i + 1), 3)
+
+for number in cubed_numbers:
+    print("Cubed ", number, " is ", cubed_numbers[number])
