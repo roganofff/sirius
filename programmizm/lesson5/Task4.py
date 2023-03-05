7571a8ad0527b065fbeb36fe07f8b7e2880bc656
roganofff <matematic1337@gmail.com>
2023-03-06 02:42:42 +0300

    task 4 solved

--- programmizm/lesson5/Task4.py
+++ programmizm/lesson5/Task4.py
@@ -3,3 +3,16 @@
 Удалите второй элемент. Добавьте в конец ключ «new_key» со значением «new_value».
 Выведите на печать итоговый словарь. Важно, чтобы словарь остался тем же (имел тот же адрес в памяти).
 """
+dictionary = {'Belarus': 'Minsk', 'Germany': 'Berlin', 'USA': 'Washington', 'Japan': 'Tokyo', 'France': 'Paris'}
+
+id_before = id(dictionary)
+
+dictionary['Belarus'], dictionary['France'] = dictionary['France'], dictionary['Belarus']
+del dictionary['Germany']
+dictionary.update({'new_key': 'new_value'})
+
+id_after = id(dictionary)
+
+print(dictionary, '\n', id_before == id_after, sep='')
+
+
