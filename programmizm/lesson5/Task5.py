1f76e04440f5323ca79878a5f608f413480e4f05
roganoff <matematic1337@gmail.com>
2023-03-06 07:56:34 +0300

    task 5 solved

--- programmizm/lesson5/Task5.py
+++ programmizm/lesson5/Task5.py
@@ -3,8 +3,12 @@
 Необходимо вывести все email-адреса в формате Имя@домен.
 """
 emails = {'mgu.edu': ['andrei_serov', 'alexander_pushkin', 'elena_belova', 'kirill_stepanov'],
-      	'gmail.com': ['alena.semyonova', 'ivan.polekhin', 'marina_abrabova'],
-      	'msu.edu': ['sergei.zharkov', 'julia_lyubimova', 'vitaliy.smirnoff'],
-      	'yandex.ru': ['ekaterina_ivanova', 'glebova_nastya'],
-      	'harvard.edu': ['john.doe', 'mark.zuckerberg', 'helen_hunt'],
-      	'mail.ru': ['roman.kolosov', 'ilya_gromov', 'masha.yashkina']}
+          'gmail.com': ['alena.semyonova', 'ivan.polekhin', 'marina_abrabova'],
+          'msu.edu': ['sergei.zharkov', 'julia_lyubimova', 'vitaliy.smirnoff'],
+          'yandex.ru': ['ekaterina_ivanova', 'glebova_nastya'],
+          'harvard.edu': ['john.doe', 'mark.zuckerberg', 'helen_hunt'],
+          'mail.ru': ['roman.kolosov', 'ilya_gromov', 'masha.yashkina']}
+
+for key, value in emails.items():
+    for name in value:
+        print(name + '@' + key)
