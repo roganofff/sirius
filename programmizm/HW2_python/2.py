6b68915d827b99906997230a75dbb6ef9c1e786d
roganofff <matematic1337@gmail.com>
2023-03-12 18:05:48 +0300

    comms added

--- programmizm/HW2_python/2.py
+++ programmizm/HW2_python/2.py
@@ -1,12 +1,12 @@
-N = int(input())
-has_Null = False
+N = int(input()) # вводится число чисел
+has_Null = False # имеется ли ноль среди чисел, по умолчанию их нет
 
-for i in range(N):
+for i in range(N): # по очереди получаем и проверяем все N чисел 
     num = int(input())
-    if num == 0:
-        has_Null = True
+    if num == 0: # если число является 0, то меняем значение перменной has_Null на истину
+        has_Null = True 
 
-if has_Null:
+if has_Null: # выводим YES или NO в зависимости от результата
     print('YES')
 else:
     print('NO')
