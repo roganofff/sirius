1c4b36854b4b73c75b6783b2244c745d602623f9
roganoff <matematic1337@gmail.com>
2023-05-05 08:21:22 +0300

    update

--- programmizm/c/homework/HW4_C/2.c
+++ programmizm/c/homework/HW4_C/2.c
@@ -7,20 +7,17 @@ int main()
     int weeks = 1 + (k - 8 + n) / 7 + ((k - 8 + n % 7 > 0) ? 1 : 0);
 
     int cnt = 1;
-    for (int i = 0; i < weeks; i++)
+    for (int i = 0; i < 7 * weeks; i++)
     {
-        for (int j = 0; j < 7; j++)
-        {
-            if (i == 0 && j < n - 1 || cnt > k) printf("  ");
-            else 
-            { 
-                (cnt < 10) ? printf(" %d", cnt) : printf("%d", cnt); 
-                cnt++; 
-            }
-            if (j < 6) printf(" ");
+        if (i < 7 && i < n - 1 || cnt > k) printf("  ");
+        else 
+        { 
+            (cnt < 10) ? printf(" %d", cnt) : printf("%d", cnt); 
+            cnt++; 
         }
-        printf("\n");
+        if (i % 7 == 0) printf(" ");
     }
+    printf("\n");
 
     return 0;
 }
