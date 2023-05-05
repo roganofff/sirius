075e4e0a5bfa6f4f129a04fe023bde3e71fb5813
roganofff <matematic1337@gmail.com>
2023-05-06 00:51:01 +0300

    comms added

--- programmizm/c/homework/HW4_C/4.c
+++ programmizm/c/homework/HW4_C/4.c
@@ -4,19 +4,20 @@
 int main()
 {
     int n;
-    scanf("%d", &n);
-    int len_binary = (int) (log2(n) + 1);
-    int binary[len_binary];
-    printf("%d\n", len_binary);
-
-    for (int i = 0; i < len_binary; i++)
+    scanf("%d", &n); // получаем данные
+    int binary_lenght = (int) (log2(n) + 1); // вычисляем длинну числа в двоичной СС, используя math.h
+    int binary[binary_lenght]; // объявляем массив длинной binary_lenght
+    // если число отрицательное, меняем его на положительное, но ставим минус в выводе
+    if (n < 0) n = -n; printf("-");
+    // т.к. при переводе таким методом нужно перевернуть число в конце, проходимся по массиву с конца
+    for (int i = binary_lenght - 1; i >= 0; i--)
     {
-        binary[len_binary - i] = n % 2;
-        n /= 2;
+        binary[i] = n % 2; // получаем одну цифру из двоичной записи числа
+        n /= 2; // делим нацело на 2, чтобы найти следующую цифру
     }
-    for (int i = 0; i < sizeof(binary) / 4; i++)
+    for (int i = 0; i < sizeof(binary) / 4; i++) // sizeof(array) / вес типа переменной == binary_lenght
     {
-        printf("%d", binary[i]);
+        printf("%d", binary[i]); // в этом цикле выводим массив, в котором лежит число в двоичной форме
     }
     
     return 0;
