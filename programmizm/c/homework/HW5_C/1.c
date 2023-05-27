fec137d24416bf983fc9903c0589634a4e7f225b
roganofff <matematic1337@gmail.com>
2023-05-27 20:54:36 +0300

    COMMS

--- programmizm/c/homework/HW5_C/1.c
+++ programmizm/c/homework/HW5_C/1.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-
-void switchHalves(int arr[], int n)
+// собснооо функция, где мы меняем половинки местами
+void switchHalves(int arr[], int n) 
 {
-    int buf, l = n / 2;
+    int buf, l = n / 2; // буф для временного хранения одного элемента, л для удобства 
     for (int k = 0; k < l; k++)
     {
-        buf = arr[k];
-        arr[k] = arr[l + k];
-        arr[l + k] = buf;
+        buf = arr[k]; // кладём значение элемента из первой части в буфер
+        arr[k] = arr[l + k]; // значение из первой части перезаписываем таким же по счёту элементом из второй чати
+        arr[l + k] = buf; // элем из второй части заменяем значением из буфера
     }
 }
 
@@ -17,7 +17,7 @@ int main()
     scanf("%d", &n);
     int arr[n];
     for (int i = 0; i < n; i++)
-    {
+    { // ясно
         scanf("%d", &arr[i]);
     }
     
